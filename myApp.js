3df79b6a67b597b188273a958247b124d2a4c5f2
Glitch (quiver-married-trust) <none>
2020-04-17 12:25:58 +0700

    DOME NEW
    NEW

--- myApp.js
+++ myApp.js
@@ -37,12 +37,18 @@ var app = express();
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-app.get('/json', function(req, res, next){
-console.log("I'm a middleware...");
-next();
-})
-/** 8) Chaining middleware. A Time server */
+// app.get('/json', function(req, res, next){
+// console.log(`${req.method} ${req.path} - ${req.ip}`);
+// next();
+// })
 
+/** 8) Chaining middleware. A Time server */
+// app.get('/now', function(req, res, next) {
+//   req.time = new Date().toString()
+//   next();
+// }, function(req, res) {
+//   res.send({time: req.time});
+// })
 
 /** 9)  Get input from client - Route parameters */
 
